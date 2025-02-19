c352bc13e87da6273a88c6cbec984dc874b6a0d3
JulienMannechez <julien.mannechez@gmail.com>
2025-02-19 11:35:58 +0100

    Premier controller de test

--- src/main/java/com/devops/spring_ci_cd_api/api/PipelineController.java
+++ src/main/java/com/devops/spring_ci_cd_api/api/PipelineController.java
@@ -2,7 +2,9 @@ package com.devops.spring_ci_cd_api.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PipelineController {
     
     @GetMapping("/pipelines/run")
