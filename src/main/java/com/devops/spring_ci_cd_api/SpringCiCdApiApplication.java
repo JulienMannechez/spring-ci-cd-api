930bbbfcbbba4bb14f9c2e619615ebe6c6984d6e
JulienMannechez <julien.mannechez@gmail.com>
2025-02-19 11:18:24 +0100

    Premier commit du projet

---
+++ src/main/java/com/devops/spring_ci_cd_api/SpringCiCdApiApplication.java
@@ -0,0 +1,13 @@
+package com.devops.spring_ci_cd_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCiCdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCiCdApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/spring_ci_cd_api/SpringCiCdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.spring_ci_cd_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCiCdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
